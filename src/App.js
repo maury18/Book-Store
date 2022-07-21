c8c02333b4c56a6dc9b0d68a42e1b9081860119f
Mauricio <tryptic-st@outlook.com>
2022-07-21 18:16:19 -0500

    Add all components in the app file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
+import Categories from './components/categories';
+import NavBar from './components/navBar';
+import Books from './components/books';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/books" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/*" element={<Books />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
