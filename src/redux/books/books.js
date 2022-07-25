b2d87c8ff361f120221afcb21a2fe52ce3a10e65
Mauricio <tryptic-st@outlook.com>
2022-07-25 17:03:57 -0500

    Add actions for the books file

---
+++ src/redux/books/books.js
@@ -0,0 +1,28 @@
+const ADD_BOOK = 'ADD_NEW_BOOK_';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+const initialState = [];
+
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
+
+    default:
+      return state;
+  }
+};
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
+
+export default booksReducer;
