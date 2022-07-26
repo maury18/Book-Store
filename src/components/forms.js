4ad9e197f1aa84f816bce6dcc8ae942242eacc84
Mauricio <tryptic-st@outlook.com>
2022-07-26 17:33:25 -0500

    Add hooks to the form

--- src/components/forms.js
+++ src/components/forms.js
@@ -1,12 +1,40 @@
-const Forms = () => (
-  <section className="form">
-    <h2>ADD NEW BOOK</h2>
-    <div className="formcontainer">
-      <input type="text" placeholder="Book title" required />
-      <input type="text" placeholder="Author" required />
-      <button type="button">Add book</button>
-    </div>
-  </section>
-);
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-export default Forms;
+const Form = () => {
+  const dispatch = useDispatch();
+  const [titleInput, setBookInput] = useState('');
+  const [authorInput, setAuthorInput] = useState('');
+
+  const handleTitle = (e) => {
+    setBookInput(e.target.value);
+  };
+  const handleAuthor = (e) => {
+    setAuthorInput(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addBook({
+      id: uuidv4(),
+      title: titleInput,
+      author: authorInput,
+    }));
+  };
+
+  return (
+    <section className="form">
+      <form className="form-container">
+        <h2>ADD NEW BOOK</h2>
+        <div className="formcontainer">
+          <input name="title" type="text" value={titleInput} onChange={(e) => handleTitle(e)} placeholder="Book title" required />
+          <input name="author" type="text" value={authorInput} onChange={(e) => handleAuthor(e)} placeholder="Author" required />
+          <button type="button" onClick={handleSubmit}>Add book</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
